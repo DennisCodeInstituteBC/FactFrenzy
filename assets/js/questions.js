4d0a015f639fef0987c4b49b8d22cae6de9cf03b
Dennis Limbu <87oyrsom@students.codeinstitute.net>
2024-08-19 16:06:07 +0000

    adding question.js

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+       numb: 1,
+       question:"What is a group of crows called ?",
+       answer:"A Murder",
+       options:[
+        "A Pack",
+        "A Murder",
+        "A Parade",
+        "A Litter"
+       ]
+    },
+    {
+        numb: 2,
+        question:"Compared to their body weight, what animal is the strongest ?",
+        answer:"Dung Beetle",
+        options:[
+         "Dung Beetle",
+         "Elephant",
+         "Ant",
+         "Cow"
+        ]
+     },
+     {
+        numb: 3,
+        question:"How many dots appear on a pair of dice ?",
+        answer:"42",
+        options:[
+         "36",
+         "32",
+         "46",
+         "42"
+        ]
+     },
+     {
+        numb: 4,
+        question:"Which is the only body part that is fully grown from birth ?",
+        answer:"Eyes",
+        options:[
+         "Barin",
+         "Lungs",
+         "Eyes",
+         "Stomach"
+        ]
+     },
+     {
+        numb: 5,
+        question:"What is acrophobia a fear of ?",
+        answer:"Heights",
+        options:[
+         "Heights",
+         "Spiders",
+         "Holes",
+         "Water"
+        ]
+     },
+]
