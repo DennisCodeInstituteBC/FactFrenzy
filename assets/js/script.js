2fae83cea829381f3490aebaee01e84bce1f6a77
Dennis Limbu <87oyrsom@students.codeinstitute.net>
2024-08-20 11:08:54 +0000

    updating css and index element and class names to allow js to work and push to next page when clicking start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 // Element Selection
-const infoSection = document.querySelector(".info-section");
-const startBtn = document.querySelector(".info-section .start-btn button");
-const quizSection = document.querySelector(".quiz-section");
-const optionList = document.querySelector(".option-list");
-const timeLine  = document.querySelector("header .time-line");
-const timeTxt   = document.querySelector("header .time-left");
-const timeNum   = document.querySelector("header .time-sec");
+const infoSection = document.querySelector('.info-section');
+const quizSection = document.querySelector('.quiz-section');
+const resultSection = document.querySelector('.result-section');
+const startBtn = document.querySelector('.start-btn button');
+const nextBtn = document.querySelector('.next-btn');
+const restartBtn = document.querySelector('.restart-btn');
+const questionTxt = document.querySelector('.question-txt');
+const optionList = document.querySelector('.option-list');
+const timerSec = document.querySelector('.time-sec');
+const totalQuestions = document.querySelector('.total-questions');
+const totalTxt = document.querySelector('.total-txt');
 
-startBtn.onclick = ()=>{
-    infoSection.classList.remove("");
-    quizSection.classList.add("")
-    showQuestions(0);
-    queCounter(1);
-    startTimer(15);
+let currentQuiz = 0;
+let score = 0;
+let timer;
+
+startBtn.onclick = () => {
+    infoSection.classList.remove('activeInfo');
+    quizSection.classList.add('activeQuiz');
+    loadQuiz();
 }
